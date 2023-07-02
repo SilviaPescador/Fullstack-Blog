27f20f5ea12ce5aaa7617f08c67530f66546f44f
SilP <pescadorlopezs@gmail.com>
2023-07-03 00:42:33 +0200

    fixed client rendering instead of server rendering

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import useSWR, {mutate} from "swr";
+import useSWR, { mutate } from "swr";
 import PostArticle from "../components/postArticle";
 import utilStyles from "../styles/utils.module.css";
 
-
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function Home() {
-	const { data, error } = useSWR("http://localhost:3001/posts", fetcher);
+export default function Home({ initialData }) {
+	const { data, error } = useSWR("http://localhost:3001/posts", fetcher, {
+		initialData: initialData || undefined,
+	});
 	if (error) return <div>Failed to load </div>;
 	if (!data) return <div>Loading...</div>;
-	
 
-	const resetPosts = async () =>{
-		mutate("http://localhost:3001/posts")
-	}
+	const resetPosts = async () => {
+		mutate("http://localhost:3001/posts");
+	};
 
 	return (
 		<Layout home>
@@ -23,13 +23,32 @@ export default function Home() {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p className="mx-3 text-center">Aquí se plasman sueños, noticias, emociones e ideas de célula y metal.</p>
+				<p className="mx-3 text-center fw-bold">
+					Aquí se plasman sueños, noticias, emociones e ideas de célula y metal.
+				</p>
 			</section>
-			<section className="py-5">
+			<section className="pb-5">
 				{data.map((post) => (
-					<PostArticle postData={post} key={post.id} onDelete={resetPosts} fullPost={false} home/>
+					<PostArticle
+						postData={post}
+						key={post.id}
+						onDelete={resetPosts}
+						fullPost={false}
+						home
+					/>
 				))}
 			</section>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch("http://localhost:3001/posts");
+	const initialData = await res.json();
+
+	return {
+		props: {
+			initialData,
+		},
+	};
 }
